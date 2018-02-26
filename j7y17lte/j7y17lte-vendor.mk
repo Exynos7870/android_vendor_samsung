af3aa8651098d6f0e162614d45fff454092b6e37
deadman96385 <seanhoyt963@gmail.com>
2018-02-26 04:28:27 -0600

    TEMP: j7y17lte import common blobs

--- j7y17lte/j7y17lte-vendor.mk
+++ j7y17lte/j7y17lte-vendor.mk
@@ -2,4 +2,4 @@
 PRODUCT_PACKAGES += libGLES_mali
 
 $(call inherit-product, vendor/samsung/j7y17lte/j7y17lte-vendor-blobs.mk)
-
+$(call inherit-product, vendor/samsung/exynos7870-common/exynos7870-common-vendor.mk)
